ff251e9abc768ebae151e6bf1c4d60d4d0c13d01
Eric Seppanen <eds@reric.net>
2021-04-22 17:00:58 -0700

    add some rust code
    This is just some useless example code for getting started.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! This is a test library. It's only here so that we can
+//! try out some rustfmt changes and clippy lint warnings.
+
+
+pub struct Foo {
+    name: String,
+    value: u64,
+}
+
+impl Foo {
+    pub fn hello(&self) -> String {
+        format!("{}: {}", self.name, self.value)
+    }
+}
+
+pub fn print_things() {
+    println!("Hello world");
+    println!("Some integers: {} {}", 42u8, 1000u32);
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
